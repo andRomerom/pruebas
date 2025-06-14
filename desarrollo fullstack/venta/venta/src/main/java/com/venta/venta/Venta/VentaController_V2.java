c643bc219035a82444c48653fb8ef1703d00050d
Aromero <and.romerom@duocuc.cl>
2025-06-14 17:27:42 -0400

    añade_hatetoas_metodo_podt_ventas

--- desarrollo fullstack/venta/venta/src/main/java/com/venta/venta/Venta/VentaController_V2.java
+++ desarrollo fullstack/venta/venta/src/main/java/com/venta/venta/Venta/VentaController_V2.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.MediaTypes;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,11 +19,35 @@ public class VentaController_V2 {
     private VentaService ventaService;
     @Autowired
     private VentaModelAssembler assembler;
-
+    //busca todas las ventas
     @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
-    public CollectionModel<EntityModel<Venta>> getAllVentas() {
-List<EntityModel<Venta>> ventas = ventaService.getAllVentas().stream().map(assembler::toModel).collect(Collectors.toList());
-return CollectionModel.of(ventas,linkTo(methodOn(VentaController_V2.class).getAllVentas()).withSelfRel());
+    public CollectionModel<EntityModel<Venta>> getAllVentas()
+     {
+           List<EntityModel<Venta>> ventas = ventaService.getAllVentas().stream().map(assembler::toModel).collect(Collectors.toList());
+            return CollectionModel.of(ventas,linkTo(methodOn(VentaController_V2.class).getAllVentas()).withSelfRel());
+     }
+     //Busca un venta por id
+     @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public EntityModel<Venta> getCarreraByCodigo(@PathVariable int id) 
+        {
+           Venta venta = ventaService.getVentaById(id)
+                .orElseThrow(() -> new RuntimeException("Venta no encontrada con ID: " + id));
+            return assembler.toModel(venta);
+        }
+
+    //Crea una nueva venta
+        @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
+       public ResponseEntity<EntityModel<Venta>> createVenta(@RequestBody Venta venta) {
+           ventaService.createVenta(venta);
+           EntityModel<Venta> entityModel = assembler.toModel(venta);
+           return ResponseEntity.created(linkTo(methodOn(VentaController_V2.class).getCarreraByCodigo(venta.getId())).toUri()).body(entityModel);
+       }
+       // public ResponseEntity<EntityModel<Venta>> getVentaById(@RequestBody Venta venta) {
+               // Venta newVenta = 
+       //        ventaService.createVenta(venta);
+       // return ResponseEntity.created(linkTo(methodOn(VentaController_V2.class).getVentaById(newVenta.getId())).toUri()).body(assembler.toModel(newVenta));
 }
 
-}
+
+
+
